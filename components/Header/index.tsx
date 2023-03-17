a8463873e42de86c181d389803d35720c5784960
higor-bianchetti <higorbianchetti@gmail.com>
2023-03-17 19:16:09 -0300

    Clean up project

---
+++ components/Header/index.tsx
@@ -0,0 +1,8 @@
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
+import styles from './styles.module.scss';
+
+export function Header() {
+  return (
+    <h1>Header works!</h1>
+  );
+}
