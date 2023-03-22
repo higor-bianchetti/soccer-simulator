cca50c3f9efeca5d7d4764eeadd6d8343043495e
higor-bianchetti <higorbianchetti@gmail.com>
2023-03-22 03:16:35 -0300

    Brasileiro Serie A table

--- pages/brasileiro-serie-a.tsx
+++ pages/brasileiro-serie-a.tsx
@@ -1,11 +1,19 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { LeagueBanner } from '../components/LeagueBanner';
+import { StandingTable } from '../components/StandingTable';
 
 import { Leagues } from '../enums/leagues';
+import { League } from '../interfaces/league';
+import { footballApi } from '../services/api';
 
 import styles from './styles.module.scss';
 
-export default function BrasileiroA() {
+interface BrasileiroAProps {
+  league: League;
+}
+
+export default function BrasileiroA({ league }: BrasileiroAProps) {
   return (
     <>
       <Head>
@@ -13,8 +21,24 @@ export default function BrasileiroA() {
       </Head>
 
       <main className={styles.main}>
-        <LeagueBanner league={Leagues.BRASILEIRO_A} season="2023" startDate='04/15/23' endDate='12/03/23' />
+        <div className={styles.banner}>
+          <LeagueBanner league={Leagues.BRASILEIRO_A} season="2023" startDate='04/15/23' endDate='12/03/23' />
+        </div>
+
+        <div className={styles.table}>
+          <StandingTable league={league} />
+        </div>
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async() => {  
+  const response = await footballApi.get('/standings?league=71&season=2023');
+
+  return {
+    props: {
+      league: response.status == 200 ? response.data.response[0].league : []
+    }
+  }
 }
