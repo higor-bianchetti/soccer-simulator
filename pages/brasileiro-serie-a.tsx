3a150c3773a3280202d0b6d6b5c16bd0f5dc909a
higor-bianchetti <higorbianchetti@gmail.com>
2023-03-23 02:56:12 -0300

    Brasileiro A top scorers and top assists

--- pages/brasileiro-serie-a.tsx
+++ pages/brasileiro-serie-a.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { LeagueBanner } from '../components/LeagueBanner';
@@ -8,12 +9,18 @@ import { League } from '../interfaces/league';
 import { footballApi } from '../services/api';
 
 import styles from './styles.module.scss';
+import { PlayerStat } from '../interfaces/playerStat';
+import { PlayerCard } from '../components/PlayerCard';
 
 interface BrasileiroAProps {
   league: League;
+  topScorers: PlayerStat[],
+  topAssists: PlayerStat[]
 }
 
-export default function BrasileiroA({ league }: BrasileiroAProps) {
+export default function BrasileiroA({ league, topScorers, topAssists }: BrasileiroAProps) {
+  const [tabActive, setTabActive] = useState(0);
+
   return (
     <>
       <Head>
@@ -26,19 +33,44 @@ export default function BrasileiroA({ league }: BrasileiroAProps) {
         </div>
 
         <div className={styles.table}>
+          <h2>Classificação</h2>
           <StandingTable league={league} />
         </div>
+
+        {(topScorers?.length && topAssists?.length) && <div className={styles.topList}>
+          <div className={styles.tabs}>
+            <div className={`${styles.tab} ${tabActive == 0 && styles.active}`} onClick={() => setTabActive(0)}>
+              <h2>Artilheiros</h2>
+            </div>
+            <div className={`${styles.tab} ${tabActive == 1 && styles.active}`} onClick={() => setTabActive(1)}>
+              <h2>Assistências</h2>
+            </div>
+          </div>
+          <div className={styles.tabContent}>
+            {tabActive == 0 && topScorers?.map((player, index) => 
+              <PlayerCard key={`sc-${index}`} pos={index + 1} player={player} score={player.statistics[0].goals.total} />
+            )}
+            {tabActive == 1 && topAssists?.map((player, index) =>
+              <PlayerCard key={`as-${index}`} pos={index + 1} player={player} score={player.statistics[0].goals.assists} />
+            )}
+          </div>
+        </div>}
       </main>
     </>
   )
 }
 
 export const getStaticProps: GetStaticProps = async() => {  
-  const response = await footballApi.get('/standings?league=71&season=2023');
+  const standingsResult = await footballApi.get('/standings?league=71&season=2023');
+  const topScorersResult = await footballApi.get('/players/topscorers?season=2023&league=71');
+  const topAssists = await footballApi.get('/players/topassists?season=2023&league=71');
 
   return {
     props: {
-      league: response.status == 200 ? response.data.response[0].league : []
-    }
+      league: standingsResult.status == 200 ? standingsResult.data.response[0].league : [],
+      topScorers: topScorersResult.status == 200 ? topScorersResult.data.response : [],
+      topAssists: topAssists.status == 200 ? topAssists.data.response : []
+    },
+    revalidate: 3600
   }
 }
